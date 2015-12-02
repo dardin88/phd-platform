eb8d12c41f9c4a4ed6a85f3a1fa95f837dcc73a5
Giuseppe <peppinos1993@gmail.com>
2015-12-02 16:02:17 +0100

    Aggiunta la gestione Avvisi

---
+++ codice/src/java/it/unisa/dottorato/News/AddNewsServlet.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.dottorato.News;
+
+import it.unisa.integrazione.model.Person;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.json.JSONException;
+import org.json.JSONObject;
+/**
+ *
+ * @author Giuseppe Picciocchi 
+ */
+@WebServlet(name = "AddNewsServlet", urlPatterns = {"/AddNewsServlet"})
+public class AddNewsServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        JSONObject result = new JSONObject();
+        PrintWriter out = response.getWriter();
+
+        try {
+            response.setContentType("text/html;charset=UTF-8");
+            
+            //conserviamo gli attributi da settare nelle variabili
+            String idNews = request.getParameter("idNews");
+            String title = request.getParameter("title");
+            String description = request.getParameter("description");
+            
+            HttpSession session = request.getSession();
+            Person loggedPerson = (Person) session.getAttribute("person");
+            
+            News news = new News();
+            
+            //inseriamo nell'oggetto corso i valori passati come parametri precedentemente
+            news.setIdNews(Integer.parseInt(idNews));
+            news.setTitle(title);
+            news.setDescription(description);
+            
+            //inseriamo l'oggetto nella gestione calendario
+            NewsManager.getInstance().insert_news(news);
+            
+            out.println("<script type=\"text/javascript\">");
+            out.println("alert('La news è stata inserita');");
+            out.println("location='collaborationActivity.jsp';"); // da modificare
+            out.println("</script>");
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(AddNewsServlet.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
