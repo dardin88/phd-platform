cc89f754b2bc2cbd0dec7732bb9344f1d39a111e
ariemmov <ergoproxy8@gmail.com>
2015-12-04 14:45:10 +0100

    In questo Commit ho ristrutturato le classi esistenti in pacchetti ognuno relativo ad una determinata Gestione, Gestione Account e Gestione Autenticazione ed Iscrizione, i nuovi pacchetti sono:  it.unisa.dottorato.account e it.unisa.dottorato.autenticazione

---
+++ codice/src/java/it/unisa/dottorato/account/Account.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.dottorato.account;
+
+/**
+ *
+ * @author ariemmov
+ */
+public class Account {
+
+    private String email;
+    private String secondaryEmail;
+    private String password;
+    private String name;
+    private String surname;
+    private String typeOfAccount;
+    private boolean isAdmin;
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+      
+    public String getSecondaryEmail() {
+        return secondaryEmail;
+    }
+    
+    public void setSecondaryEmail(String secondary) {
+        secondaryEmail = secondary;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getSurname() {
+        return surname;
+    }
+    
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }  
+
+    public void setTypeOfAccount(String typeOfAccount) {
+        this.typeOfAccount = typeOfAccount;
+    }
+
+    public String getTypeOfAccount() {
+        return typeOfAccount;
+    }
+    
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+    
+    public void setAdmin(boolean var) {
+        isAdmin = var;
+    }
+}
---
+++ codice/src/java/it/unisa/dottorato/account/PhdStudent.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.dottorato.account;
+
+/**
+ *
+ * @author ariemmov
+ */
+public class PhdStudent extends Account {
+    private final String type = "dottorando";
+    private String telephone;
+    private String link;
+    private String department;
+    private String researchInterest;
+    private String fkCurriculum;
+    private int fkCycle;
+    private String fkProfessor;
+    private String fkAccount;
+
+ 
+    @Override
+    public String getTypeOfAccount() {
+        return type;
+    }
+
+ 
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getResearchInterest() {
+        return researchInterest;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+ 
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setResearchInterest(String researchInterest) {
+        this.researchInterest = researchInterest;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public int getfkCycle() {
+        return fkCycle;
+    }
+
+    public String getfkCurriculum() {
+        return fkCurriculum;
+    }
+
+    public String getfkProfessor() {
+        return fkProfessor;
+    }
+
+    public void setfkCurriculum(String FK_curriculum) {
+        this.fkCurriculum = FK_curriculum;
+    }
+
+    public void setfkProfessor(String FK_professor) {
+        this.fkProfessor = FK_professor;
+    }
+
+    public void setfkCycle(int FK_cycle) {
+        this.fkCycle = FK_cycle;
+    }
+
+    public String getfkAccount() {
+        return fkAccount;
+    }
+
+    public void setfkAccount(String FK_account) {
+        this.fkAccount = FK_account;
+    }
+    
+    
+    
+    
+    
+    
+     
+    
+}
