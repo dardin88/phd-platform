162d9768d747e288fc7e9ed514e3c85733aee3df
tminichiello <minichiellounisa@gmail.com>
2016-01-09 16:38:04 +0100

    aggiunta test presenze

--- codice/test/it/unisa/dottorato/presence/PresenceManagerTest.java
+++ codice/test/it/unisa/dottorato/presence/PresenceManagerTest.java
@@ -5,13 +5,8 @@
  */
 package it.unisa.dottorato.presence;
 
-import it.unisa.dottorato.account.Account;
-import it.unisa.dottorato.phdCourse.Course;
-import java.util.ArrayList;
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -45,122 +40,370 @@ public class PresenceManagerTest {
         assertNotNull(result);
     }
 
-    /**
-     * Test of getCourseByProfessor method, of class PresenceManager.
-     */
     @Test
-    public void testGetCourseByProfessor() throws Exception {
-        System.out.println("getCourseByProfessor");
-        String Professor = "";
-        PresenceManager instance = null;
-        ArrayList<Course> expResult = null;
-        ArrayList<Course> result = instance.getCourseByProfessor(Professor);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of insertPresence method, of class PresenceManager.
-     */
-    @Test
-    public void testInsertPresence() throws Exception {
-        System.out.println("insertPresence");
-        Presence dottorando = null;
-        PresenceManager instance = null;
-        instance.insertPresence(dottorando);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPresenceList method, of class PresenceManager.
-     */
-    @Test
-    public void testGetPresenceList() throws Exception {
-        System.out.println("getPresenceList");
-        int lesson = 0;
-        PresenceManager instance = null;
-        ArrayList<Presence> expResult = null;
-        ArrayList<Presence> result = instance.getPresenceList(lesson);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPresenceDottorandi method, of class PresenceManager.
-     */
-    @Test
-    public void testGetPresenceDottorandi() throws Exception {
-        System.out.println("getPresenceDottorandi");
-        int idCorso = 0;
-        PresenceManager instance = null;
-        ArrayList<Account> expResult = null;
-        ArrayList<Account> result = instance.getPresenceDottorandi(idCorso);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPresenceToLesson method, of class PresenceManager.
-     */
-    @Test
-    public void testGetPresenceToLesson() throws Exception {
-        System.out.println("getPresenceToLesson");
-        String dottorando = "";
-        int idCorso = 0;
-        PresenceManager instance = null;
-        ArrayList<Presence> expResult = null;
-        ArrayList<Presence> result = instance.getPresenceToLesson(dottorando, idCorso);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of modifyPresence method, of class PresenceManager.
-     */
-    @Test
-    public void testModifyPresence() throws Exception {
-        System.out.println("modifyPresence");
-        String dottorando = "";
-        int idLesson = 0;
-        PresenceManager instance = null;
-        instance.modifyPresence(dottorando, idLesson);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of testDottorando method, of class PresenceManager.
-     */
-    @Test
-    public void testTestDottorando() throws Exception {
-        System.out.println("testDottorando");
-        String title = "";
-        PresenceManager instance = null;
-        String expResult = "";
-        String result = instance.testDottorando(title);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of testid method, of class PresenceManager.
-     */
-    @Test
-    public void testTestid() throws Exception {
-        System.out.println("testid");
-        int id = 0;
-        PresenceManager instance = null;
-        int expResult = 0;
-        int result = instance.testid(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testGetCourseByProfessorOk(){
+        String Professor = "adelucia@hotmail.it";
+        try{
+            instance.getCourseByProfessor(Professor);
+            assertTrue(true);
+        }catch(Exception e){
+            fail("non sono riuscito a fare l' op");
+        }
     }
     
+    @Test
+    public void testGetCourseByProfessorProfessorMin(){
+        String Professor = "";
+        try{
+            instance.getCourseByProfessor(Professor);
+            fail("non sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testGetCourseByProfessorProfessorMax(){
+        String Professor = "qwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiop";
+        try{
+            instance.getCourseByProfessor(Professor);
+            fail("non sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+     
+    @Test
+    public void testGetCourseByProfessorProfessorFormatError(){
+        String Professor = "qwertyuiopqwpqwertyuiopqwertyuiop";
+        try{
+            instance.getCourseByProfessor(Professor);
+            fail("non sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testInsertPresenceOk(){
+        pre.setFkLesson(1);
+        pre.setFkPhdstudent("ballo@hotmail.it");
+        pre.setIsPresent(true);
+        try{
+            instance.insertPresence(pre);
+            assertTrue(true);
+        }catch(Exception e){
+            fail("non sono riuscito a creare l' op");
+        }
+    }
+    
+    @Test
+    public void testInsertPresenceLessonMin(){
+        pre.setFkLesson(-9);
+        pre.setFkPhdstudent("ballo@hotmail.it");
+        pre.setIsPresent(true);
+        try{
+            instance.insertPresence(pre);
+            fail("sono riuscito a creare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testInsertPresenceLessonMax(){
+        pre.setFkLesson(9999999);
+        pre.setFkPhdstudent("ballo@hotmail.it");
+        pre.setIsPresent(true);
+        try{
+            instance.insertPresence(pre);
+            fail("sono riuscito a creare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testInsertPresencefkPhdstudentMin(){
+        pre.setFkLesson(1);
+        pre.setFkPhdstudent("");
+        pre.setIsPresent(true);
+        try{
+            instance.insertPresence(pre);
+            fail("sono riuscito a creare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testInsertPresencefkPhdstudentMax(){
+        pre.setFkLesson(1);
+        pre.setFkPhdstudent("qwertyuopqwertyuopqwertyuopqwertyuopqwertyuopqwertyuop");
+        pre.setIsPresent(true);
+        try{
+            instance.insertPresence(pre);
+            fail("sono riuscito a creare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testInsertPresencefkPhdstudentFormatError(){
+        pre.setFkLesson(1);
+        pre.setFkPhdstudent("qwertyuopqwertyuo@wertyuopqwertyuop");
+        pre.setIsPresent(true);
+        try{
+            instance.insertPresence(pre);
+            fail("sono riuscito a creare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testGetPresenceListOk(){
+        int lesson = 1;
+        try{
+            instance.getPresenceList(lesson);
+            assertTrue(true);
+        }catch(Exception e){
+            fail("non sono riuscito a fare l' op");
+        }
+    }
+        
+    @Test
+    public void testGetPresenceListLessonMin(){
+        int lesson = -41;
+        try{
+            instance.getPresenceList(lesson);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testGetPresenceListLessonMax(){
+        int lesson = 98989889;
+        try{
+            instance.getPresenceList(lesson);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testGetPresenceListLessonNotExists(){
+        int lesson = 98989;
+        try{
+            instance.getPresenceList(lesson);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testGetPresenceDottorandiOk(){
+        int idCorso = 1;
+        try{
+            instance.getPresenceDottorandi(idCorso);
+            assertTrue(true);
+        }catch(Exception e){
+            fail("non sono riuscito a fare l' op");
+        }
+    }
+    
+    @Test
+    public void testGetPresenceDottorandiCourseMin(){
+        int idCorso = -89;
+        try{
+            instance.getPresenceDottorandi(idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testGetPresenceDottorandiCourseMax(){
+        int idCorso = 9898989;
+        try{
+            instance.getPresenceDottorandi(idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+    
+    @Test
+    public void testGetPresenceDottorandiCourseNotExists(){
+        int idCorso = 9898;
+        try{
+            instance.getPresenceDottorandi(idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    public void testGetPresenceToLesson(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = 1;
+        try{
+            instance.getPresenceToLesson(dottorando, idCorso);
+            assertTrue(true);
+        }catch(Exception e){
+            fail("non sono riuscito a fare l' op");
+        }    
+    }
+    
+    @Test
+    public void testGetPresenceToLessonDottorandoMin(){
+        String dottorando = "";
+        int idCorso = 1;
+        try{
+            instance.getPresenceToLesson(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testGetPresenceToLessonDottorandoMax(){
+        String dottorando = "qwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiop";
+        int idCorso = 1;
+        try{
+            instance.getPresenceToLesson(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testGetPresenceToLessonDottorandoFormatError(){
+        String dottorando = "qwertyuiopqpqwertyuiopqwertyuiop";
+        int idCorso = 1;
+        try{
+            instance.getPresenceToLesson(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testGetPresenceToLessonCorsoMin(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = -8;
+        try{
+            instance.getPresenceToLesson(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testGetPresenceToLessonCorsoMax(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = 9898989;
+        try{
+            instance.getPresenceToLesson(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+
+    @Test
+    public void testModifyPresence(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = 1;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            assertTrue(true);
+        }catch(Exception e){
+            fail("sono riuscito a fare l' op");
+        }    
+    }
+    
+    
+    @Test
+    public void testModifyPresenceDottorandoMin(){
+        String dottorando = "";
+        int idCorso = 1;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testModifyPresenceDottorandoMax(){
+        String dottorando = "qwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiop";
+        int idCorso = 1;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testModifyPresenceDottorandoFormatError(){
+        String dottorando = "qwertyuiopqpqwertyuiopqwertyuiop";
+        int idCorso = 1;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testModifyPresenceCorsoMin(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = -8;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testModifyPresenceCorsoMax(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = 9898989;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
+    
+    @Test
+    public void testModifyPresenceNotExists(){
+        String dottorando = "dinucci@hotmail.it";
+        int idCorso = 878;
+        try{
+            instance.modifyPresence(dottorando, idCorso);
+            fail("sono riuscito a fare l' op");
+        }catch(Exception e){
+            assertTrue(true);
+        }    
+    }
 }
