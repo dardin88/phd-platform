ea6503bd89c02ede73e9d5a52b39cd28bb4c592a
Ernesto <Ernesto@172.19.3.51>
2016-05-06 14:40:52 +0200

    Aggiunta classe Activity

--- phdPlatform/src/main/java/it/unisa/dottorato/activityRegister/Activity.java
+++ phdPlatform/src/main/java/it/unisa/dottorato/activityRegister/Activity.java
@@ -5,10 +5,87 @@
  */
 package it.unisa.dottorato.activityRegister;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  *
  * @author Ernesto Pizza
  */
 public class Activity {
     
+    private int idActivity;
+    private String name;
+    private String description;
+    private Date startDateTime;
+    private Date endDateTime;
+    private float totalTime;
+    private String typology;
+    private String fkPhdStudent;    
+
+    public int getIdActivity() {
+        return idActivity;
+    }
+
+    public void setIdActivity(int idActivity) {
+        this.idActivity = idActivity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDateTime() {
+        return startDateTime;
+    }
+
+    public void setStartDateTime(Date startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public Date getEndDateTime() {
+        return endDateTime;
+    }
+
+    public void setEndDateTime(Date endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public float getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(float totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public String getTypology() {
+        return typology;
+    }
+
+    public void setTypology(String typology) {
+        this.typology = typology;
+    }
+
+    public String getFkPhdStudent() {
+        return fkPhdStudent;
+    }
+
+    public void setFkPhdStudent(String fkPhdStudent) {
+        this.fkPhdStudent = fkPhdStudent;
+    }
+    
+
 }
